04e7e37bb88979032db6f3f5de05b6403c744a76
Kaustav Halder <96991352+JadedSoftwares@users.noreply.github.com>
2024-07-13 06:13:54 +0000

    First Commit. AES Encryption cone.

---
+++ JadedEncryption/KeyGen.cs
@@ -0,0 +1,45 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace JadedEncryption;
+
+public enum KeySize
+{
+    KeySize_128 = 16,
+    KeySize_198 = 24,
+    KeySize_256 = 32
+}
+
+public static class KeyGen
+{
+    private static readonly char[] chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()-_=+<>?".ToCharArray();
+
+    private static string GenerateRandomString(int length)
+    {
+        byte[] data = new byte[4 * length];
+        using (RNGCryptoServiceProvider crypto = new RNGCryptoServiceProvider())
+        {
+            crypto.GetBytes(data);
+        }
+
+        StringBuilder result = new StringBuilder(length);
+        for (int i = 0; i < length; i++)
+        {
+            uint rnd = BitConverter.ToUInt32(data, i * 4);
+            long idx = rnd % chars.Length;
+            result.Append(chars[idx]);
+        }
+
+        return result.ToString();
+    }
+
+    public static string GenerateAesKey(KeySize keySize)
+    {
+        return GenerateRandomString(Convert.ToInt32(keySize));
+    }
+
+    public static string GenerateIv()
+    {
+        return GenerateRandomString(Convert.ToInt32(KeySize.KeySize_128));
+    }
+}
---
+++ TestConsole/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+using JadedEncryption;
+
+
+Console.WriteLine("Testing One Way Encription");
+OnewayEncryption oneWay = new OnewayEncryption();
+string toEncript = "this is a demo string";
+
+string encrypedStuff = oneWay.HashData(toEncript);
+
+Console.WriteLine("Data to Encrypt = " + toEncript);
+Console.WriteLine("Encryped Data: " + encrypedStuff);
+
+if(oneWay.VerifyHash(encrypedStuff, toEncript)) {
+    Console.WriteLine("This one passed");
+}
+
+if(!oneWay.VerifyHash(encrypedStuff, "HELLO WORLD"))
+{
+    Console.WriteLine("This one should have failed");
+}
+
+
+Console.WriteLine("Testing Two Way Encription");
+
+string key = KeyGen.GenerateAesKey(KeySize.KeySize_256);
+string iv = KeyGen.GenerateIv();
+
+Console.WriteLine("Key is " + key);
+Console.WriteLine("IV is " + iv);
+
+TwoWayEncryption twoWay = new TwoWayEncryption(key, iv);
+string twoWayEncryptedString = twoWay.Encrypt("HELLO WORLD");
+string twoWayDecryptedString = twoWay.Decrypt(twoWayEncryptedString);
+
+Console.WriteLine("Two way encryptedString : " + twoWayEncryptedString);
+Console.WriteLine("Two way decrypted string : " + twoWayDecryptedString);
+
+
+Console.ReadLine();
